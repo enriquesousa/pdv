820734dfb3ee53837ac4c486d2bfe1cd4e503ae3
Enrique <enrique.sousa@gmail.com>
2024-01-30 07:36:42 -0800

    Mejora: 109a Custom Register Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,15 +36,9 @@
                                 <div class="text-center w-75 m-auto">
                                     <div class="auth-logo">
 
-                                        <a href="index.html" class="logo logo-dark text-center">
+                                        <a href="{{ url('/') }}" class="logo logo-dark text-center">
                                             <span class="logo-lg">
                                                 <img src="{{ asset('backend/assets/images/logo-dark2.png')}}" alt="" height="22">
-                                            </span>
-                                        </a>
-                    
-                                        <a href="index.html" class="logo logo-light text-center">
-                                            <span class="logo-lg">
-                                                <img src="{{ asset('backend/assets/images/logo-light2.png')}}" alt="" height="22">
                                             </span>
                                         </a>
 
