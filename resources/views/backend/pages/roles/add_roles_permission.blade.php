824f5f337401925a310e4caa618d332f09aa3150
Enrique <enrique.sousa@gmail.com>
2024-01-29 19:17:10 -0800

    S36 210. Select All check con JS - Add Roles in Permission Part 3

--- resources/views/backend/pages/roles/add_roles_permission.blade.php
+++ resources/views/backend/pages/roles/add_roles_permission.blade.php
@@ -3,6 +3,13 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
+    {{-- Para Capitalizar la primer letra de todos los labels de los check boxes --}}
+    <style type="text/css">
+        .form-check-label{
+            text-transform: capitalize;
+        }
+    </style>
+
     <div class="content">
 
         <!-- Start Content-->
@@ -56,8 +63,8 @@
                                         <div class="row">
                                             <div class="col-md-6">
                                                 <div class="form-check mb-2 form-check-primary">
-                                                    <input class="form-check-input" type="checkbox" value="" id="customckeck15">
-                                                    <label class="form-check-label" for="customckeck15">Primary</label>
+                                                    <input class="form-check-input" type="checkbox" value="" id="customCheckAll">
+                                                    <label class="form-check-label" for="customCheckAll">Seleccionar Todo</label>
                                                 </div>
                                             </div>
                                         </div>
@@ -116,5 +123,15 @@
 
     </div> <!-- content -->
 
+    {{-- JS para seleccionar todos los checkbox --}}
+    <script type="text/javascript">
+        $('#customCheckAll').click(function(){
+            if ($(this).is(':checked')) {
+                $('input[type = checkbox]').prop('checked',true);
+            }else{
+                $('input[type = checkbox]').prop('checked',false);
+            } 
+        });
+   </script>
 
 @endsection
