d624f9b3e2d036366feff0ffd89e68e0aa139740
Enrique <enrique.sousa@gmail.com>
2024-02-22 09:38:08 -0800

    Bandera Mexico

--- resources/views/body/header.blade.php
+++ resources/views/body/header.blade.php
@@ -158,31 +158,39 @@
 
             {{-- BANDERAS DE IDIOMAS --}}
             <li class="dropdown d-none d-lg-inline-block topbar-dropdown">
+
+                {{-- Bandera por defecto --}}
                 <a class="nav-link dropdown-toggle arrow-none waves-effect waves-light" data-bs-toggle="dropdown"
                     href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <img src="{{ asset('backend/assets/images/flags/us.jpg') }}" alt="user-image" height="16">
+                    <img src="{{ asset('backend/assets/images/flags/Mexico.png') }}" alt="user-image" height="16">
                 </a>
+
                 <div class="dropdown-menu dropdown-menu-end">
 
-                    <!-- item-->
+                    <!-- USA -->
+                    <a href="javascript:void(0);" class="dropdown-item">
+                        <img src="{{ asset('backend/assets/images/flags/us.jpg') }}" alt="user-image" class="me-1" height="12"> <span class="align-middle">USA</span>
+                    </a>
+
+                    <!-- germany -->
                     <a href="javascript:void(0);" class="dropdown-item">
                         <img src="{{ asset('backend/assets/images/flags/germany.jpg') }}" alt="user-image"
                             class="me-1" height="12"> <span class="align-middle">German</span>
                     </a>
 
-                    <!-- item-->
+                    <!-- italy -->
                     <a href="javascript:void(0);" class="dropdown-item">
                         <img src="{{ asset('backend/assets/images/flags/italy.jpg') }}" alt="user-image"
                             class="me-1" height="12"> <span class="align-middle">Italian</span>
                     </a>
 
-                    <!-- item-->
+                    <!-- spain -->
                     <a href="javascript:void(0);" class="dropdown-item">
                         <img src="{{ asset('backend/assets/images/flags/spain.jpg') }}" alt="user-image"
                             class="me-1" height="12"> <span class="align-middle">Spanish</span>
                     </a>
 
-                    <!-- item-->
+                    <!-- russia -->
                     <a href="javascript:void(0);" class="dropdown-item">
                         <img src="{{ asset('backend/assets/images/flags/russia.jpg') }}" alt="user-image"
                             class="me-1" height="12"> <span class="align-middle">Russian</span>
